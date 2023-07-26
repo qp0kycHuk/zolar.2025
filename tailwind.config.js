990c27132670f06c6b5d405dc05ec667dbb8dc5c
qp0kycHuk <serega990112@mail.ru>
2023-07-26 20:41:31 +0300

    some fixes

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,39 +13,36 @@ const headingStyles = {
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./src/**/*.{html,js,vue}'],
-  darkMode: 'class',
+  darkMode: ['class', '[data-theme="dark"]'],
   theme: {
     screens: {
       xs: 420 + 29.98 + 'px',
       sm: 580 + 29.98 + 'px',
       md: 720 + 29.98 + 'px',
       lg: 1170 + 29.98 + 'px',
-      // 'xl': (1170 + 29.98) + 'px',
+      xl: 1270 + 29.98 + 'px',
+    },
+    container: {
+      xs: 420 + 'px',
+      sm: 580 + 'px',
+      md: 720 + 'px',
+      lg: 1170 + 'px',
+      xl: 1366 + 'px',
     },
     colors: {
-      primary: {
-        DEFAULT: '#b71540',
-      },
-      sec: {
-        DEFAULT: '#CE9B44',
-      },
-      blue: {
-        DEFAULT: '#2980b9',
-      },
-      red: {
-        DEFAULT: '#c0392b',
-      },
-      green: {
-        DEFAULT: '#27ae60',
-      },
-      yellow: {
-        DEFAULT: '#f39c12',
-      },
-      gray: {
-        DEFAULT: '#333',
-      },
+      primary: '#b71540',
+      sec: '#CE9B44',
+      blue: '#2980b9',
+      red: '#c0392b',
+      green: '#27ae60',
+      yellow: '#f39c12',
+      gray: '#333',
       white: '#fff',
       black: '#000',
+    },
+    fontFamily: {
+      base: 'var(--font-base)',
+      alt: 'var(--font-alt)',
     },
     zIndex: [0, 321, 322, 323, 324, 325, 326, 327, 328, 329, 'auto'],
     extend: {
@@ -56,26 +53,18 @@ module.exports = {
         l2: 'var(--bg2)',
         l3: 'var(--bg3)',
       },
-      typography: {
-        DEFAULT: {
-          css: {
-            h1: headingStyles,
-            h2: headingStyles,
-            h3: headingStyles,
-            h4: headingStyles,
-            h5: headingStyles,
-            h6: headingStyles,
-            b: headingStyles,
-            strong: headingStyles,
-          },
-        },
+      fontSize: {
+        '1.5xl': ['1.375rem', '1.35'],
+        '2.1xl': ['1.5625rem', '1.35'],
+        '2.5xl': ['1.75rem', '1.35'],
+        '3.5xl': ['2rem', '1.35'],
+        '4.5xl': ['2.5rem', '1.35'],
       },
     },
   },
   plugins: [
-    require('@tailwindcss/typography'),
     require('@qpokychuk/tailwind-button-plugin'),
-    require('@qpokychuk/tailwind-image-ratio-plugin'),
+    require('@qpokychuk/tailwind-ratio-plugin'),
     require('./tailwind.form.js'),
   ],
 }
