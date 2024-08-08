eff4b04af086cf0cc80f234db685664c7244b0f6
qp0kycHuk <42573149+qp0kycHuk@users.noreply.github.com>
2024-08-08 09:16:48 +0300

    Update scrollTo.ts

--- src/js/scrollTo.ts
+++ src/js/scrollTo.ts
@@ -3,25 +3,27 @@ function clickHandler(event: MouseEvent) {
 
   if (!target) return
 
-  const href = target?.getAttribute('data-scroll')
+  const href = target?.getAttribute('href')
 
   if (!href) return
-  if (href[0] != '#' || href == '#') return
+  if ((href[0] != '#' && !href.startsWith('/#')) || href == '#') return
 
-  event.preventDefault()
-
-  const element = document.querySelector(href)
+  const selector = href.replace('/#', '#')
+  const element = document.querySelector(selector)
 
   if (!element) return
+  event.preventDefault()
   const offset = 45
   const bodyRect = document.body.getBoundingClientRect().top
   const elementRect = element.getBoundingClientRect().top
   const elementPosition = elementRect - bodyRect
   const offsetPosition = elementPosition - offset
 
-  window.scrollTo({
-    top: offsetPosition,
-    behavior: 'smooth',
+  setTimeout(() => {
+    window.scrollTo({
+      top: offsetPosition,
+      behavior: 'smooth',
+    })
   })
 }
 
