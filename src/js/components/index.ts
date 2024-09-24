57c1b00857344a214db77a208d2a3f8423d7a5b8
qp0kycHuk <serega990112@mail.ru>
2024-09-24 17:49:23 +0300

    refactoring menu

--- src/js/components/index.ts
+++ src/js/components/index.ts
@@ -1,17 +1,11 @@
 import counter from './counter'
 import icon from './icon'
-import menu from './menu/menu'
-import menuButton from './menu/menuButton'
-import menuItem from './menu/menuItem'
-import menuItems from './menu/menuItems'
+import menu from '@qpokychuk/wc-menu'
 
 function registerAll() {
   counter.register()
   icon.register()
   menu.register()
-  menuItems.register()
-  menuButton.register()
-  menuItem.register()
 }
 
 registerAll()
