50e14c4e4b2b7401437c2a968b5f15a87910daad
qp0kycHuk <serega990112@mail.ru>
2024-10-16 10:09:44 +0300

    reset to default tpl

--- src/js/index.ts
+++ src/js/index.ts
@@ -8,7 +8,6 @@ import toggle from 'npm-kit-toggle'
 import ripple from '@qpokychuk/ripple'
 import swiper from './swiper'
 import animations from './animations'
-import CanvasSnow from '@cycjimmy/canvas-snow'
 
 import '../scss/index.scss'
 
@@ -26,37 +25,14 @@ function loadHandler() {
   ripple.deAttach('.btn-text')
 
   animations.init()
+  swiper.init()
+  showPass.init()
+  theme.init()
+  fancybox.init()
 
   scrollHandler()
   document.addEventListener('toggleopen', toggleOpenHandler)
   document.addEventListener('toggleclose', toggleCloseHandler)
-
-  const ball = document.querySelector('.ball') as HTMLElement
-
-  ball.addEventListener('click', () => {
-    ball.classList.toggle('shuffle')
-  })
-
-  const snow1 = ball.querySelector('.ball-snow-1') as HTMLCanvasElement
-  const snow2 = ball.querySelector('.ball-snow-2') as HTMLCanvasElement
-  const snow3 = ball.querySelector('.ball-snow-3') as HTMLCanvasElement
-
-  const canvasSnow1 = new CanvasSnow({
-    context: snow1,
-    cell: 300,
-  }).init()
-  const canvasSnow2 = new CanvasSnow({
-    context: snow2,
-    cell: 300,
-  }).init()
-  const canvasSnow3 = new CanvasSnow({
-    context: snow3,
-    cell: 300,
-  }).init()
-
-  canvasSnow1.start()
-  canvasSnow2.start()
-  canvasSnow3.start()
 }
 
 window.addEventListener('scroll', scrollHandler)
